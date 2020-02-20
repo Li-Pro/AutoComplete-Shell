c722dce38cce34f68b7b8555a3e00f97e93a4ffd
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-02-20 11:52:46 +0800

    Adding advanced key parsing.

--- acshell.cpp
+++ acshell.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<windows.h>
 #include<conio.h>
+#include<cctype>
 #include<vector>
 
 CHAR_INFO dst[5*5];
@@ -10,24 +11,19 @@ void raise(std::string errMsg, int exitCode) { std::cerr<<errMsg; exit(exitCode)
 void shell()
 {
 	const int TAB = 9, BACK = 8, RET = 13;
-//	const int TAB = 9;
-//	const int BACK = 8;
-//	const int RET = 13;
 	
 	// TODO: Error handle
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (hOut == INVALID_HANDLE_VALUE) exit(1);
 	
-	CONSOLE_SCREEN_BUFFER_INFO scrInfo;
-	
-//	std::cout<<GetLastError()<<'\n';
-	if (!GetConsoleScreenBufferInfo(hOut, &scrInfo))
-	{
-		std::cout<<GetLastError()<<std::endl;
-	}
-//	exit(1);
-	
-	COORD pos = scrInfo.dwCursorPosition;
+//	CONSOLE_SCREEN_BUFFER_INFO scrInfo;
+//	
+//	if (!GetConsoleScreenBufferInfo(hOut, &scrInfo))
+//	{
+//		std::cout<<GetLastError()<<std::endl;
+//	}
+//	
+//	COORD pos = scrInfo.dwCursorPosition;
 	
 	const auto writeAt = [&](std::string v, int x, int y) -> unsigned long
 	{
@@ -39,22 +35,62 @@ void shell()
 	int key=-1, typed=0;
 	while ((key=getch()) != RET)
 	{
-		int px = pos.X+typed, py = pos.Y;
+		CONSOLE_SCREEN_BUFFER_INFO scrInfo;
+		
+		if (!GetConsoleScreenBufferInfo(hOut, &scrInfo))
+		{
+			std::cout<<GetLastError()<<std::endl;
+		}
+		
+		COORD pos = scrInfo.dwCursorPosition;
+		
+		int px = pos.X, py = pos.Y;
+//		std::cout<<std::hex<<'('<<key<<')'<<std::flush;
 		if (key==BACK)
 		{
 			if (typed <= 0) continue;
-//			std::cout<<"<-";
 			writeAt(" ", px-1, py);
 			SetConsoleCursorPosition(hOut, {(short)(px-1), (short)py});
 			typed--;
 		}
-		else if (key!=TAB)
+		else if (key==TAB)
 		{
-//			std::cout<<(char)key, typed++;
-//			std::cout<<(char)key;
+			
+		}
+		else if (isprint(key))
+		{
 			writeAt(std::string()+(char)key, px, py);
 			SetConsoleCursorPosition(hOut, {(short)(px+1), (short)py});
 			typed++;
+		}
+		else
+		{
+			const auto sendKey = [](int key, int extFlag) -> void
+			{
+//				std::cout<<"##"<<(char)key<<' '<<(char)(key^0xE0)<<std::flush;
+//				std::cout<<"#"<<extFlag<<std::flush;
+				INPUT ip;
+				ip.type = INPUT_KEYBOARD;
+				ip.ki.wScan = 0;
+				ip.ki.time = 0;
+				ip.ki.dwExtraInfo = 0;
+				ip.ki.wVk = key;
+				
+				ip.ki.dwFlags = 0 | extFlag; // key press
+				SendInput(1, &ip, sizeof(ip));
+				
+				ip.ki.dwFlags = KEYEVENTF_KEYUP | extFlag;
+				SendInput(1, &ip, sizeof(ip));
+			};
+			
+			int extFlag = 0;
+			if (key==0xE0)
+			{
+				extFlag |= KEYEVENTF_EXTENDEDKEY; key = getch();
+			}
+//			std::cout<<"#####"<<std::flush;
+			sendKey(key, extFlag);
+//			sendKey('K', extFlag);
 		}
 		// TODO: key > 26
 	}
@@ -64,8 +100,6 @@ void shell()
 
 void read(std::string &v, std::vector<std::string> pool)
 {
-//	int x;
-//	while ((x=getch())!=26) std::cout<<x<<' '<<(char)x<<'\n';
 	shell();
 	
 	const auto filter = [&](std::string x, std::string pat) -> bool
